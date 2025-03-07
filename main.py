e920ae70a2f872c01c08e75ad5158127ce1cbeee
Kontuch Suksawat <zeroxr20@gmail.com>
2025-03-07 23:09:45 +0700

    change main2 to main

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ def print_numbers(name):
         time.sleep(1)
 
 
+def print_10_numbers(name):
+    for i in range(1, 11):
+        print(f"{name}: {i}")
+        time.sleep(1)
+
+
 thread1 = threading.Thread(target=print_numbers, args=("Thread-1",))
-thread2 = threading.Thread(target=print_numbers, args=("Thread-2",))
+thread2 = threading.Thread(target=print_10_numbers, args=("Thread-2",))
 
 thread1.start()
 thread2.start()
